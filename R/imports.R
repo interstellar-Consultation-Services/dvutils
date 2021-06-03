ff71b8af2ba88179b25f457c1666e00470a04b8f
Mohammed Ali <moh_fcis@yahoo.com>
2021-06-03 05:50:16 +0200

    developed build_adjacency_matrix

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,4 @@
 #' @import dplyr
+#' @import tidyr
 #' @importFrom assertthat assert_that
 NULL
--- R/tools.R
+++ R/tools.R
@@ -1,10 +1,10 @@
-#' Builds drugs target association matrix from given drugs_targets dataframe
+#' Builds adjacency matrix from given data between source and target columns
 #'
 #' The dataframe must contains drugs and targets columns.
 #'
-#' @param drugs_targets dataframe that contains drugs_targets data
-#' @param drugs_column name of drugs column in the given dataset
-#' @param  targets_column name of targets column in the given dataset
+#' @param data dataframe that contains data data
+#' @param source name of drugs column in the given dataset
+#' @param targets name of targets column in the given dataset
 #' @return dataframe with following sepecifications:
 #'          * first column as drugs ids
 #'          * targets count other columns (with targets ids as headers)
@@ -13,37 +13,43 @@
 #'            * 0 there is no relation.
 #' @examples
 #' \dontrun{
-#'   build_drgus_target_matrix(drugs_targets = dbdataset::Targets_Drug,
-#'                             drugs_column = parent_key,
-#'                             targets_column = id)
+#'   build_adjacency_matrix(data = dbdataset::Targets_Drug,
+#'                          source = "parent_key",
+#'                          targets = "id")
 #' }
 #' @export
-build_drgus_target_matrix <- function(drugs_targets,
-                                      drugs_column,
-                                      targets_column) {
+build_adjacency_matrix <- function(data,
+                                   source,
+                                   target) {
   assertthat::assert_that(
-    !missing(drugs_targets),
-    !is.null(drugs_targets),
-    "data.frame" %in% class(drugs_targets),
-    nrow(drugs_targets) > 0,
-    msg = "drugs_targets must be a data.frame that contains"
+    !missing(data),
+    !is.null(data),
+    "data.frame" %in% class(data),
+    nrow(data) > 0,
+    msg = "data must be a data.frame that contains"
   )
   assertthat::assert_that(
-    !missing(drugs_column),
-    !is.null(drugs_column),
-    is.character(drugs_column),
-    drugs_column != "",
-    drugs_column %in% names(drugs_targets),
-    msg = paste("drugs_column must be a character value reprsents drugs column",
+    !missing(source),
+    !is.null(source),
+    is.character(source),
+    source != "",
+    source %in% names(data),
+    msg = paste("source must be a character value reprsents drugs column",
                 "name in drugs_target data.frame")
   )
   assertthat::assert_that(
-    !missing(targets_column),
-    !is.null(drugs_column),
-    is.character(targets_column),
-    targets_column != "",
-    targets_column %in% names(drugs_targets),
-    msg = paste("targets_column must be a character value reprsents drugs column",
+    !missing(target),
+    !is.null(source),
+    is.character(target),
+    target != "",
+    target %in% names(data),
+    msg = paste("target must be a character value reprsents drugs column",
                 "name in drugs_target data.frame")
   )
+  # not all drugs has targets, what we will do with other drugs?!!!
+  # investigate more later
+  data %>%
+    group_by(!!sym(source), !!sym(target)) %>%
+    tally() %>%
+    spread(!!sym(target), n, drop = FALSE, fill = 0)
 }
