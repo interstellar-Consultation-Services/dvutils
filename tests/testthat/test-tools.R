36c3c116f7714feb9c5259977676c16a1c51c905
Mohammed Ali <moh_fcis@yahoo.com>
2021-06-26 14:43:04 +0200

    add build_adjacency_matrix unit tests

--- tests/testthat/test-tools.R
+++ tests/testthat/test-tools.R
@@ -12,48 +12,55 @@ test_that("build_adjacency_matrix", {
                regexp = msg)
   expect_error(build_adjacency_matrix(12),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix[, 1]),
+  expect_error(build_adjacency_matrix(Targets_Drug[, 1]),
                regexp = msg)
 
   ## test source
   msg <- "source must be a character value reprsents a valid data column name"
-  expect_error(build_adjacency_matrix(drugs_targets_matrix),
+  expect_error(build_adjacency_matrix(Targets_Drug),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix, NULL),
+  expect_error(build_adjacency_matrix(Targets_Drug, NULL),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix, NA),
+  expect_error(build_adjacency_matrix(Targets_Drug, NA),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix, "Not_column"),
+  expect_error(build_adjacency_matrix(Targets_Drug, "Not_column"),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix, 123),
+  expect_error(build_adjacency_matrix(Targets_Drug, 123),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix, c("parent_key",
+  expect_error(build_adjacency_matrix(Targets_Drug, c("parent_key",
                                                               "ID")),
                regexp = msg)
 
   ## test target
   msg <- "target must be a character value reprsents a valid data column name"
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key"),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key",
                                       NULL),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key",
                                       NA),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key",
                                       "Not_column"),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key",
                                       123),
                regexp = msg)
-  expect_error(build_adjacency_matrix(drugs_targets_matrix,
+  expect_error(build_adjacency_matrix(Targets_Drug,
                                       source = "parent_key",
                                       c("parent_key", "ID")),
                regexp = msg)
+
+  # test data
+  mat <- build_adjacency_matrix(Targets_Drug,
+                                source = "parent_key",
+                                target = "id")
+  expect_equal(mat[[1, "parent_key"]], "DB00112")
+  expect_equal(mat[[1, "BE0000163"]], 1)
 })
