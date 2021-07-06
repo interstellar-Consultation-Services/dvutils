c51e1831871cfdc0745cd02e876c41e08071feba
Mohammed Ali <moh_fcis@yahoo.com>
2021-07-06 09:48:49 +0200

    fixed descriptors calculations

--- R/drugs.R
+++ R/drugs.R
@@ -26,7 +26,7 @@ drug_descriptors_categories <- function() {
 #' }
 #' @export
 drug_descriptors_names <- function(category = "all") {
-  if (!all(!missing(category),
+  if (!all(!is.na(category),
            !is.null(category),
            is.character(category),
            all(category %in% c("all",drug_descriptors_categories())))) {
@@ -39,46 +39,143 @@ drug_descriptors_names <- function(category = "all") {
   rcdk::get.desc.names(category)
 }
 
+#' Builds adjacency matrix from given data between source and target columns
+#'
+#' The dataframe must contains drugs and targets columns.
+#'
+#' @param data dataframe that contains data data
+#' @param source name of drugs column in the given dataset
+#' @param targets name of targets column in the given dataset
+#' @return dataframe with following sepecifications:
+#'          * first column as drugs ids
+#'          * targets count other columns (with targets ids as headers)
+#'          * values for these columns will be either:
+#'            * 1 there is a relation,
+#'            * 0 there is no relation.
+#' @examples
+#' \dontrun{
+#'   build_adjacency_matrix(data = dbdataset::Targets_Drug,
+#'                          source = "parent_key",
+#'                          targets = "id")
+#' }
 #' @export
-calculate_drugs_AIO <- function(smiles, id_column, smile_column, which.desc) {
-  #asserts and dynamic columns
-  f <- data.frame()
-  sm <- smiles %>% dplyr::select(!!rlang::sym(smile_column)) %>% pull()
-  id <- smiles %>% dplyr::select(!!rlang::sym(id_column)) %>% pull()
-  for (i in 1:nrow(smiles)) {
-    f <- f %>% dplyr::bind_rows(calculate_smile_features(sm[i],
-                                                         id[i],
-                                                         which.desc))
+calculate_drugs_AIO <- function(smiles, id_column, smile_column, descriptors) {
+  assertthat::assert_that(
+    !missing(smiles),
+    !is.null(smiles),
+    "data.frame" %in% class(smiles),
+    nrow(smiles) > 0,
+    ncol(smiles) >= 2,
+    msg = "smiles must be a dataframe that contains at least two columns"
+  )
+
+  assertthat::assert_that(
+    !missing(id_column),
+    !is.null(id_column),
+    is.character(id_column),
+    length(id_column) == 1,
+    id_column != "",
+    id_column %in% names(smiles),
+    msg = "id_column must be a character value reprsents a valid smiles column name"
+  )
+
+  assertthat::assert_that(
+    !missing(smile_column),
+    !is.null(smile_column),
+    is.character(smile_column),
+    length(smile_column) == 1,
+    smile_column != "",
+    smile_column %in% names(smiles),
+    msg ="smile_column must be a character value reprsents a valid smiles column name"
+  )
+  descriptors_3d <- c("org.openscience.cdk.qsar.descriptors.molecular.CPSADescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.GravitationalIndexDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.IPMolecularLearningDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.LengthOverBreadthDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.MomentOfInertiaDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.VABCDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.WHIMDescriptor",
+                      "org.openscience.cdk.qsar.descriptors.molecular.PetitjeanShapeIndexDescriptor")
+
+  calculate_3d <- any(descriptors %in% descriptors_3d)
+  if (calculate_3d &&
+      !"ChemmineR" %in% rownames(installed.packages())) {
+    warning(paste("'ChemmineR' package is needed for calculating",
+                  "3D molecules data.",
+                  "Disabling descriptors that needs 3D data"))
+    calculate_3d <- FALSE
   }
-  f[ifelse(sapply(f, function(x)all(is.na(x))) == TRUE, TRUE, FALSE)] <- NULL
-  f[ifelse(sapply(f, function(x)all(is.nan(x))) == TRUE, TRUE, FALSE)] <- NULL
-  f[ifelse(sapply(f, function(x)all(x==0)) == TRUE, TRUE, FALSE)] <- NULL
-  # f<- f %>% mutate(across(everything(), .fns = ~tidyr::replace_na(.,0)))
-  rownames(f) <- f[["drug"]]
-  f
+
+  if (calculate_3d) {
+    descriptors_1d <- setdiff(descriptors, descriptors_3d)
+    descriptors_3d <-descriptors[descriptors %in% descriptors_3d]
+  }
+
+  features <- data.frame()
+  sm <- smiles %>% dplyr::select(!!dplyr::sym(smile_column)) %>% dplyr::pull()
+  id <- smiles %>% dplyr::select(!!dplyr::sym(id_column)) %>% dplyr::pull()
+  if (calculate_3d) {
+    for (i in 1:nrow(smiles)) {
+      features <- features %>%
+        dplyr::bind_rows(calculate_smile_features(sm[i], id[i], descriptors_1d))
+    }
+
+    features_3d <- data.frame()
+    for (i in 1:nrow(smiles)) {
+      features_3d <- features_3d %>%
+        dplyr::bind_rows(
+          calculate_smile_features(sm[i], id[i], descriptors_3d, TRUE))
+    }
+    features <- merge(features, features_3d)
+
+  } else {
+    for (i in 1:nrow(smiles)) {
+      features <- features %>%
+        dplyr::bind_rows(calculate_smile_features(sm[i], id[i], descriptors))
+    }
+  }
+
+  features[ifelse(sapply(features, function(x)all(is.na(x))) == TRUE, TRUE, FALSE)] <- NULL
+  features[ifelse(sapply(features, function(x)all(is.nan(x))) == TRUE, TRUE, FALSE)] <- NULL
+  features[ifelse(sapply(features, function(x)all(x==0)) == TRUE, TRUE, FALSE)] <- NULL
+  features[, -c(1,2)] <- useful::simple.impute.data.frame(features[, -c(1,2)])#features %>% mutate(across(everything(), .fns = ~tidyr::replace_na(.,0)))
+  rownames(features) <- features[["drug"]]
+  features
 }
 
-calculate_smile_features <- function(smile, drug_id, which.desc) {
+calculate_smile_features <- function(smile,
+                                     drug_id,
+                                     descriptor,
+                                     require_3d = FALSE) {
   tryCatch({
-    # check for ChemminerR existence?
-    sdf <- ChemmineR::smiles2sdf(smile)
-    sdf <- ChemmineR::generate3DCoords(sdf)
-    tmpdest <- tempfile(pattern='xxx')
-    ChemmineR::write.SDF(sdf, tmpdest)
-    farr <- rJava::.jarray(tmpdest, contents.class = 'S')
-    molecules <- rJava::.jcall('org/guha/rcdk/util/Misc',
-                               '[Lorg/openscience/cdk/interfaces/IAtomContainer;',
-                               'loadMolecules',
-                               farr,
-                               TRUE,
-                               TRUE,
-                               TRUE,
-                               check = FALSE)
-    cbind(drug = drug_id, smile, rcdk::eval.desc(molecules, which.desc))
+    molecule <- R.utils::withTimeout(get_molecules(smile, require_3d),
+                                     timeout = 180)
+    cbind(drug = drug_id, smile, rcdk::eval.desc(molecule, descriptor))
   },
 
   error = function(e) {
     NA
   })
+}
+
+get_molecules <- function(smile, require_3d = FALSE) {
+  if (require_3d) {
+    sdf <- ChemmineR::smiles2sdf(smile)
+    sdf <- R.utils::withSink(ChemmineR::generate3DCoords(sdf),
+                             file = tempfile())
+    tmpdest <- tempfile(pattern='xxx')
+    ChemmineR::write.SDF(sdf, tmpdest)
+    farr <- rJava::.jarray(tmpdest, contents.class = 'S')
+    rJava::.jcall('org/guha/rcdk/util/Misc',
+                  '[Lorg/openscience/cdk/interfaces/IAtomContainer;',
+                  'loadMolecules',
+                  farr,
+                  TRUE,
+                  TRUE,
+                  TRUE,
+                  check = FALSE)
+  } else {
+    rcdk::parse.smiles(smile)
+  }
 }
 
